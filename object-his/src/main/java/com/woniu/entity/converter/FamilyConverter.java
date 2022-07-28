00d54cb91974f02d1033209964d964dc735a6098
xuke <1975594056@qq.com>
2022-07-28 10:48:09 +0800

    xk修改bug

--- object-his/src/main/java/com/woniu/entity/converter/FamilyConverter.java
+++ object-his/src/main/java/com/woniu/entity/converter/FamilyConverter.java
@@ -3,11 +3,16 @@ package com.woniu.entity.converter;
 import com.woniu.entity.dto.FamilyDto;
 import com.woniu.entity.po.FamilyPo;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface FamilyConverter {
+
+    @Mapping(source = "id",target = "id")
+    @Mapping(source = "familyname",target = "familyname")
+    List<FamilyDto> listFrom(List<FamilyPo> familyPoList);
 
     FamilyDto from(FamilyPo po);
 
